b811b12b39e1fec90b539dcfec9cc9d1081bd11a
ranggaperwiratama <thenorthmarketkid@gmail.com>
2016-03-31 14:26:17 +0700

    insert jalan
    insert jalan

--- demo_phonegap-master/server/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-// header("Access-Control-Allow-Origin: *");
- mysql_connect("localhost","root","");
- mysql_select_db("phonegap_demo");
-?>
--- demo_phonegap-master/server/json.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "db.php";
-$data=array();
-$q=mysql_query("select * from `course_details`");
-while ($row=mysql_fetch_object($q)){
- $data[]=$row;
-}
-echo json_encode($data);
-?>
